756d08b5008895fcf5177114cf2ada68e22391ec
Fabrizzio20k <fabrizzio785@gmail.com>
2024-04-30 22:23:50 -0500

    Add Lab 4

--- DATABASE II/PROYECTOS/PROYECTO 1/main.cpp
+++ DATABASE II/PROYECTOS/PROYECTO 1/main.cpp
@@ -1,110 +1,22 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-
-using namespace std;
-
-struct BucketRecord
-{
-    int BucketSize;
-    int GlobalDepth;
-
-    string idx;
-    vector<long> positions;
-    long next;
-
-    friend ostream &operator<<(ostream &os, const BucketRecord &b)
-    {
-        os.write((char *)&b.idx, b.idx.size());
-        for (size_t i = 0; i < b.positions.size(); i++)
-        {
-            os.write((char *)&b.positions[i], sizeof(long));
-        }
-        os.write((char *)&b.next, sizeof(long));
-        return os;
-    }
-    friend istream &operator>>(istream &is, BucketRecord &b)
-    {
-        is.read((char *)&b.idx, b.GlobalDepth);
-        for (size_t i = 0; i < b.BucketSize; i++)
-        {
-            is.read((char *)&b.positions[i], sizeof(long));
-        }
-        is.read((char *)&b.next, sizeof(long));
-        return is;
-    }
-};
-
-struct Person
-{
-    string name;
-    int age;
-    double weight;
-};
-
-class ExtendibleHashing
-{
-public:
-    static void clearFile(string file)
-    {
-        ofstream ofs;
-        ofs.open(file, ofstream::out | ofstream::trunc);
-        ofs.close();
-    }
-    ExtendibleHashing(string bucketFile, string directoryFile, int bucketSize, int globalDepth = 3)
-    {
-        this->bucketFile = bucketFile;
-        this->directoryFile = directoryFile;
-        this->bucketSize = bucketSize;
-        this->globalDepth = globalDepth;
-
-        ExtendibleHashing::clearFile(bucketFile);
-
-        ofstream file(bucketFile);
-        // Write the initial buckets
-        for (int i = 0; i < 1 << globalDepth; i++)
-        {
-            BucketRecord bucket;
-            bucket.idx = to_string(i);
-            bucket.next = -1;
-            for (int j = 0; j < bucketSize; j++)
-            {
-                bucket.positions.push_back(-1);
-            }
-            file << bucket;
-        }
-    }
-    void insert(Person p);
-    void remove(string name);
-    void search(string name);
-    void display();
-    void displayBuckets()
-    {
-        ifstream file(bucketFile);
-        BucketRecord bucket;
-        bucket.BucketSize = bucketSize;
-        bucket.GlobalDepth = globalDepth;
-        while (file >> bucket)
-        {
-            cout << bucket.idx << " ";
-            for (size_t i = 0; i < bucket.positions.size(); i++)
-            {
-                cout << bucket.positions[i] << " ";
-            }
-            cout << bucket.next << endl;
-        }
-    }
-
-private:
-    int globalDepth;
-    int bucketSize;
-    string bucketFile;
-    string directoryFile;
-};
+#include "sqlparser.h"
 
 int main()
 {
-    ExtendibleHashing eh("index.dat", "data.dat", 2, 5);
-    eh.displayBuckets();
+    SQLParser parser;
+    parser.parse("create table students from file \"students.txt\" using AVL Index (\"id\")");
+    parser.printSentence();
+
+    parser.parse("select * from students where id = 123");
+    parser.printSentence();
+
+    parser.parse("select * from students where id between 123 and 456");
+    parser.printSentence();
+
+    parser.parse("insert into students values (123, \"pepe\", 20)");
+    parser.printSentence();
+
+    parser.parse("delete from students where id = 123");
+    parser.printSentence();
+
+    return 0;
 }
---
+++ DATABASE II/PROYECTOS/PROYECTO 1/sqlparser.h
@@ -0,0 +1,207 @@
+#ifndef SQLPARSER_H
+#define SQLPARSER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+enum class CommandType
+{
+    CREATE,
+    SELECT,
+    RANGE,
+    INSERT,
+    DELETE,
+    UNKNOWN
+};
+
+struct Command
+{
+    CommandType type;
+    string table;
+    string condition;
+    string route;
+    string indexType;
+    string indexColumn;
+    string r1, r2;
+    vector<string> values;
+};
+
+class SQLParser
+{
+private:
+    string command;
+    Command cmd;
+    void parseCommand()
+    {
+        regex createRegex(R"(create table (\w+) from file \"(.+)\" using (index hash|ISAM|AVL Index) \(\"(\w+)\"\))");
+        regex selectRegex(R"(select \* from (\w+) where (\w+) = (.+))");
+        regex rangeRegex(R"(select \* from (\w+) where (\w+) between (.+) and (.+))");
+        regex insertRegex(R"(insert into (\w+) values \((.+)\))");
+        regex deleteRegex(R"(delete from (\w+) where (\w+) = (.+))");
+
+        smatch matches;
+
+        if (regex_search(command, matches, createRegex))
+        {
+            cmd.type = CommandType::CREATE;
+            cmd.table = matches[1];
+            cmd.route = matches[2];
+            cmd.indexType = matches[3];
+            cmd.indexColumn = matches[4];
+        }
+        else if (regex_search(command, matches, selectRegex))
+        {
+            cmd.type = CommandType::SELECT;
+            if (cmd.table != matches[1])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw std::invalid_argument("Table name in select command does not match the table name in the command");
+            }
+            if (cmd.indexColumn != matches[2])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw std::invalid_argument("Index column in select command does not match the index column in the command");
+            }
+            cmd.condition = matches[3];
+        }
+        else if (regex_search(command, matches, rangeRegex))
+        {
+            cmd.type = CommandType::RANGE;
+            if (cmd.table != matches[1])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw std::invalid_argument("Table name in select range command does not match the table name in the command");
+            }
+            if (cmd.indexColumn != matches[2])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw std::invalid_argument("Index column in select range command does not match the index column in the command");
+            }
+            cmd.r1 = matches[3];
+            cmd.r2 = matches[4];
+        }
+        else if (regex_search(command, matches, insertRegex))
+        {
+            cmd.type = CommandType::INSERT;
+            if (cmd.table != matches[1])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw std::invalid_argument("Table name in insert command does not match the table name in the command");
+            }
+            string values = matches[2];
+            string value;
+            for (char c : values)
+            {
+                if (c == ',')
+                {
+                    cmd.values.push_back(value);
+                    value.clear();
+                }
+                else
+                {
+                    value += c;
+                }
+            }
+            cmd.values.push_back(value);
+        }
+        else if (regex_search(command, matches, deleteRegex))
+        {
+            cmd.type = CommandType::DELETE;
+            if (cmd.table != matches[1])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw invalid_argument("Table name in delete command does not match the table name in the command");
+            }
+            if (cmd.indexColumn != matches[2])
+            {
+                cmd.type = CommandType::UNKNOWN;
+                throw invalid_argument("Index column in delete command does not match the index column in the command");
+            }
+
+            cmd.condition = matches[3];
+        }
+        else
+        {
+            cmd.type = CommandType::UNKNOWN;
+        }
+    }
+
+public:
+    SQLParser() {}
+
+    Command getCommand()
+    {
+        return cmd;
+    }
+
+    void parse(const string &command)
+    {
+        this->command = command;
+        parseCommand();
+    }
+
+    void printSentence()
+    {
+        switch (cmd.type)
+        {
+        case CommandType::CREATE:
+            std::cout << "Creating table " << cmd.table << " from file " << cmd.route << " using " << cmd.indexType << " (" << cmd.indexColumn << ")\n";
+            break;
+        case CommandType::SELECT:
+            std::cout << "Selecting from table " << cmd.table << " where " << cmd.indexColumn << " = " << cmd.condition << "\n";
+            break;
+        case CommandType::RANGE:
+            std::cout << "Selecting from table " << cmd.table << " where " << cmd.indexColumn << " between " << cmd.r1 << " and " << cmd.r2 << "\n";
+            break;
+        case CommandType::INSERT:
+            std::cout << "Inserting into table " << cmd.table << " values (";
+            for (int i = 0; i < cmd.values.size(); i++)
+            {
+                std::cout << cmd.values[i];
+                if (i < cmd.values.size() - 1)
+                {
+                    std::cout << ", ";
+                }
+            }
+            std::cout << ")\n";
+            break;
+        case CommandType::DELETE:
+            std::cout << "Deleting from table " << cmd.table << " where " << cmd.indexColumn << " = " << cmd.condition << "\n";
+            break;
+        default:
+            std::cout << "Unknown command\n";
+        }
+    }
+
+    void testAll()
+    {
+        Command cmd;
+        string command;
+
+        command = "create table students from file \"students.txt\" using AVL Index (\"id\")";
+        parseCommand();
+        printSentence();
+
+        command = "select * from students where id = 123pepe";
+        parseCommand();
+        printSentence();
+
+        command = "select * from students where id between 1pepe and 123pepe";
+        parseCommand();
+        printSentence();
+
+        command = "insert into students values (123, 'John', 'Doe', 'CS')";
+        parseCommand();
+        printSentence();
+
+        command = "delete from students where id = 123";
+        parseCommand();
+        printSentence();
+    }
+};
+
+#endif // !SQLPARSER_H
