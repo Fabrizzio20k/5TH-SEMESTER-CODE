1b931ccc8cdea6beb0bdc5d9f835749b892a98b8
Fabrizzio20k <fabrizzio785@gmail.com>
2024-05-14 15:19:44 -0500

    Delete project files

--- MACHINE LEARNING/PROYECTO_2/main.py
+++ MACHINE LEARNING/PROYECTO_2/main.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
 import h5py
 import numpy as np
 
@@ -30,3 +32,20 @@ with h5py.File('test.h5', 'r') as f:
     data_test_X = np.array(data_test_X)
 
     print(data_test_X.shape)
+
+# Aplana los datos
+data_train_X = data_train_X.reshape(data_train_X.shape[0], -1)
+data_test_X = data_test_X.reshape(data_test_X.shape[0], -1)
+
+# use decision tree, get accuracy and get confusion matrix
+
+# Entrena el modelo
+model = DecisionTreeClassifier()
+model.fit(data_train_X, data_train_Y)
+
+# Realiza predicciones
+predictions = model.predict(data_test_X)
+
+# Obtiene la exactitud
+accuracy = accuracy_score(data_train_Y, predictions)
+print("Accuracy: %.2f" % accuracy)
