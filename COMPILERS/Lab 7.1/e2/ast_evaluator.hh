d5e3644726ce66183d1a58df6fb5f6b2ce2a6db0
Fabrizzio <fabrizzio.vilchez.e@tutec.edu.pe>
2024-05-14 12:07:24 -0500

    Add files

---
+++ COMPILERS/Lab 7.1/e2/ast_evaluator.hh
@@ -0,0 +1,18 @@
+#ifndef AST_EVALUATOR
+#define AST_EVALUATOR
+
+#include "exp.hh"
+#include "ast_visitor.hh"
+
+class ASTEvaluator : public ASTVisitor {
+public:
+  int eval(Exp*);
+  // metodos visit
+
+  int visit(BinaryExp *e);
+    int visit(NumberExp *e);
+    int visit(ParenthExp *e);
+};
+
+
+#endif
