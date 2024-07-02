aed2fb7a328adee671c85d48834a27f289f462dc
Fabrizzio Vilchez <115495332+Fabrizzio20k@users.noreply.github.com>
2024-07-02 10:31:54 -0500

    Fix ident

--- COMPILERS/Lab 13.1/imp_printer.cpp
+++ COMPILERS/Lab 13.1/imp_printer.cpp
@@ -1,182 +1,152 @@
 #include "imp_printer.hh"
-#include <iomanip>
 
-void ImpPrinter::print(Program *p) {
+void ImpPrinter::print(Program* p) {
   p->accept(this);
   return;
 }
 
-void ImpPrinter::visit(Program *p) {
+void ImpPrinter::visit(Program* p) {
   p->var_decs->accept(this);
   p->fun_decs->accept(this);
   return;
 }
 
-void ImpPrinter::visit(Body *b) {
+void ImpPrinter::visit(Body * b) {
   b->var_decs->accept(this);
   b->slist->accept(this);
   return;
 }
 
-void ImpPrinter::visit(VarDecList *s) {
-  list<VarDec *>::iterator it;
+void ImpPrinter::visit(VarDecList* s) {
+  list<VarDec*>::iterator it;
   for (it = s->vdlist.begin(); it != s->vdlist.end(); ++it) {
-    cout << setw(indent * 2) << "" << flush;
     (*it)->accept(this);
     cout << ";" << endl;
-  }
+  }  
   return;
 }
 
-void ImpPrinter::visit(FunDecList *s) {
-  list<FunDec *>::iterator it;
+void ImpPrinter::visit(FunDecList* s) {
+  list<FunDec*>::iterator it;
   for (it = s->fdlist.begin(); it != s->fdlist.end(); ++it) {
-    cout << setw(indent * 2) << "" << flush;
     (*it)->accept(this);
     cout << endl;
   }
   return;
 }
 
-void ImpPrinter::visit(FunDec *fd) {
+void ImpPrinter::visit(FunDec* fd) {
   cout << "fun " << fd->rtype << " " << fd->fname << "(";
   bool first = true;
   list<string>::iterator it, vit;
-  for (it = fd->types.begin(), vit = fd->vars.begin(); it != fd->types.end();
-       ++it, ++vit) {
-    if (!first)
-      cout << ", ";
+  for (it = fd->types.begin(), vit = fd->vars.begin();
+       it != fd->types.end(); ++it, ++vit){
+    if (!first) cout << ", ";
     first = false;
     cout << *it << " " << *vit;
   }
   cout << ")" << endl;
-
-  indent++;
   fd->body->accept(this);
-  indent--;
-  cout << setw(indent * 2) << "" << flush;
   cout << "endfun";
   return;
 }
-
-void ImpPrinter::visit(VarDec *vd) {
+			  
+void ImpPrinter::visit(VarDec* vd) {
   bool first = true;
   cout << "var " << vd->type << " ";
   list<string>::iterator it;
-  for (it = vd->vars.begin(); it != vd->vars.end(); ++it) {
-    if (!first)
-      cout << ", ";
+  for (it = vd->vars.begin(); it != vd->vars.end(); ++it){
+    if (!first) cout << ", ";
     first = false;
     cout << *it;
   }
   return;
 }
 
-void ImpPrinter::visit(StatementList *s) {
-  list<Stm *>::iterator it;
+void ImpPrinter::visit(StatementList* s) {
+  list<Stm*>::iterator it;
   for (it = s->slist.begin(); it != s->slist.end(); ++it) {
-    cout << setw(indent * 2) << "" << flush;
     (*it)->accept(this);
     cout << ";" << endl;
   }
   return;
 }
 
-void ImpPrinter::visit(AssignStatement *s) {
+void ImpPrinter::visit(AssignStatement* s) {
   cout << s->id << " = ";
   s->rhs->accept(this);
   return;
 }
 
-void ImpPrinter::visit(PrintStatement *s) {
+void ImpPrinter::visit(PrintStatement* s) {
   cout << "print(";
   s->e->accept(this);
   cout << ")";
   return;
 }
 
-void ImpPrinter::visit(IfStatement *s) {
-  cout << "if (";
+void ImpPrinter::visit(IfStatement* s) {
+  cout << "if";
   s->cond->accept(this);
-  cout << ") then" << endl;
-  ;
-  indent++;
-
+  cout << " then" << endl;;
   s->tbody->accept(this);
-
-  indent--;
-
-  cout << setw(indent * 2) << "" << flush;
-  if (s->fbody != NULL) {
+  if (s->fbody!=NULL) {
     cout << "else" << endl;
-
-    indent++;
-
     s->fbody->accept(this);
-
-    indent--;
   }
-  cout << setw(indent * 2) << "" << flush;
   cout << "endif";
   return;
 }
 
-void ImpPrinter::visit(WhileStatement *s) {
-  cout << "while (";
+void ImpPrinter::visit(WhileStatement* s) {
+  cout << "while ";
   s->cond->accept(this);
-  cout << ") do" << endl;
-  ;
-  indent++;
+  cout << "do" << endl;;
   s->body->accept(this);
-  indent--;
-  cout << setw(indent * 2) << "" << flush;
   cout << "endwhile";
   return;
 }
 
-void ImpPrinter::visit(ReturnStatement *s) {
+void ImpPrinter::visit(ReturnStatement* s) {
   cout << "return (";
-  if (s->e != NULL)
-    s->e->accept(this);
+  if (s->e != NULL) s->e->accept(this);
   cout << ")";
   return;
 }
 
 // Expresiones
 
-int ImpPrinter::visit(BinaryExp *e) {
+int ImpPrinter::visit(BinaryExp* e) {
   e->left->accept(this);
   cout << ' ' << Exp::binopToString(e->op) << ' ';
   e->right->accept(this);
   return 0;
 }
 
-int ImpPrinter::visit(NumberExp *e) {
+int ImpPrinter::visit(NumberExp* e) {
   cout << e->value;
   return 0;
 }
 
-int ImpPrinter::visit(TrueFalseExp *e) {
-  if (e->value)
-    cout << "true";
-  else
-    cout << "false";
+int ImpPrinter::visit(TrueFalseExp* e) {
+  if (e->value) cout << "true";
+  else cout << "false";
   return 0;
 }
 
-int ImpPrinter::visit(IdExp *e) {
+int ImpPrinter::visit(IdExp* e) {
   cout << e->id;
   return 0;
 }
 
-int ImpPrinter::visit(ParenthExp *ep) {
+int ImpPrinter::visit(ParenthExp* ep) {
   cout << '(';
   ep->e->accept(this);
   cout << ')';
   return 0;
 }
 
-int ImpPrinter::visit(CondExp *e) {
+int ImpPrinter::visit(CondExp* e) {
   cout << "ifexp(";
   e->cond->accept(this);
   cout << ",";
@@ -187,15 +157,15 @@ int ImpPrinter::visit(CondExp *e) {
   return 0;
 }
 
-int ImpPrinter::visit(FCallExp *e) {
+int ImpPrinter::visit(FCallExp* e) {
   cout << e->fname << "(";
-  list<Exp *>::iterator it;
+  list<Exp*>::iterator it;
   bool first = true;
   for (it = e->args.begin(); it != e->args.end(); ++it) {
-    if (!first)
-      cout << ",";
+    if (!first) cout << ",";
     first = false;
     (*it)->accept(this);
+    
   }
   cout << ')';
   return 0;
